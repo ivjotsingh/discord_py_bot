8455ef93c39c2bb37b90ff28f5facf5f4db753eb
ivjotsingh <ivjotofficial@gmail.com>
2020-05-30 19:11:52 +0530

    google custom search REST API testing

--- commands/google.py
+++ commands/google.py
@@ -1,10 +1,17 @@
 import requests
+from decouple import config
 
 
+class GoogleCommand:
+    @staticmethod
+    def execute(message, args):
+        query = " ".join(args)
 
+        google_url = f"https://www.googleapis.com/customsearch/v1?key={config('GOOGLE_KEY')}&" \
+            f"cx={config('GOOGLE_ENGINE_ID')}&&q={query}&&num=5"
+        response = requests.get(google_url).json()
+        response.raise_for_status()
 
+        for data in response.items:
+            print(data)
 
-class GoogleCommand():
-
-    def execute(self, message, args):
-        pass
