f471610fa1864aa6fa373dc3fcfdf21e4d47cf06
ivjotsingh <ivjotofficial@gmail.com>
2020-05-31 00:55:36 +0530

    no of google results set in environemnt so that code should not be changed in case of inc or dec

--- utilities/google_search.py
+++ utilities/google_search.py
@@ -3,6 +3,7 @@ from decouple import config
 
 
 def search_query(query):
+    no_of_results = config('NO_OF_GOOGLE_RESULTS')
     google_url = f"https://www.googleapis.com/customsearch/v1?key={config('GOOGLE_KEY')}&" \
-        f"cx={config('GOOGLE_ENGINE_ID')}&&q={query}&&num=5"
+        f"cx={config('GOOGLE_ENGINE_ID')}&&q={query}&&num=f{no_of_results}"
     return requests.get(google_url)
