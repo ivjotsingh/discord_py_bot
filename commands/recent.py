baafafc44521d88ad3939eed9f794a0309ae200a
ivjotsingh <ivjotofficial@gmail.com>
2020-05-31 00:16:57 +0530

    recent command functionality created

--- commands/recent.py
+++ commands/recent.py
@@ -1,8 +1,14 @@
-import requests
+from utilities.mongo import fetch_from_mongo_db
 
 
 class RecentCommand():
 
     @staticmethod
     def execute(args):
-        pass
+        query = " ".join(args)
+        results = ", ".join(fetch_from_mongo_db(query))
+
+        return [{
+                'heading': f'Recent Searches for {query}',
+                'content': results}
+            ]
