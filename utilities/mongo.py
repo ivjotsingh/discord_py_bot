665a72edb9547c1cce9839722a4b1e76b8bc0f4d
ivjotsingh <ivjotofficial@gmail.com>
2020-05-31 00:57:46 +0530

    Utilities refactored for better understanding

--- utilities/mongo.py
+++ utilities/mongo.py
@@ -7,14 +7,17 @@ from decouple import config
 # connect to MongoDB cluster
 cluster = MongoClient(config('MONGO_DB_URL'))
 
+# db name is discord
 db = cluster['discord']
+
+# collection in the db is of name search
 collection = db['search']
 
 
-def post_to_mongo_db(data):
+def post_to_mongo_db(data):     # function pushes a row in collection search
     collection.insert_one(data)
 
 
-def fetch_from_mongo_db(query):
+def fetch_from_mongo_db(query):     # function fetches distinct queries from history which matches pattern of query
     regx = re.compile(f"{query}", re.IGNORECASE)
     return collection.find({"query": regx}).distinct("query")
